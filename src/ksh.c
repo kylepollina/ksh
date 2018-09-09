1719d0d64539aa6186efe55f705d42ec7cf3546e
Kyle Pollina <kylepollina@gmail.com>
2018-09-08 23:33:21 -0400

    v1.0 version 1.0. supported commands: help, cd, exit

--- src/ksh.c
+++ src/ksh.c
@@ -6,8 +6,8 @@
 
 int main(int argc, char **argv)
 {
-    printf("Type HELP for more options...\n");
-    printf("Type Q to quit\n");
+    printf("Type help for more options...\n");
+    printf("Type exit to quit\n");
 
     ksh();	
 
@@ -21,18 +21,14 @@ void ksh()
     int status = TRUE;      // loop status
 
     do {
-        printf("sh_kyle>> ");
+        printf("ksh> ");
         fgets(input_str, MAXBUF, stdin);
 
         char **args = ksh_split_line(input_str);
 
-        if(strcmp(args[0], "q") == 0 || strcmp(args[0], "Q") == 0){
-            status = FALSE;
-        }
-
         status = ksh_execute(args);
 
-    } while(status); 
+    } while(!status);
 
     free(input_str);
 }
@@ -67,66 +63,6 @@ int ksh_launch(char **args)
         } while (!WIFEXITED(status) && !WIFSIGNALED(status));
     }
 
-    return 1;    
+    return 0;    
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// BUILT IN COMMANDS. USER MADE COMMANDS GO IN /src/usrfunc.c
-
-// HELP or help 
-/* void help(list_t *arguments) */ 
-/* { */
-/* 	if(listLength(arguments) >= 1){ */
-/* 		node_t *arg1 = arguments->head->next; */
-
-/* 		if(arg1->id == ID_STR && strcmp(arg1->str, "create") == 0){ */
-/* 			printf("\ncreate -- create can be used to create a datastructure.\n"); */
-/* 			printf("To create a doubly linked use the command\n"); */
-/* 			printf("	create list\n\n"); */
-/* 		} */
-
-/* 		else{ */
-/* 			printf("--help: argument not recognized. Try running 'help' or 'HELP'\n"); */
-/* 		} */
-/* 	} */
-
-/* 	else{ */
-/* 		printf("===============================================\n"); */
-/* 		printf(" sh_kyle - A 'shell' style command prompt used\n"); */
-/* 		printf("           for testing and running programs\n"); */
-/* 		printf("           I've made.\n"); */
-/* 		printf("===============================================\n"); */
-/* 		printf("                              By: Kyle Pollina\n"); */
-/* 		printf("These are the commands you can run...\n\n"); */
-
-/* 		printf("<command>\t\t\tDescription\n"); */
-/* 		printf("create <arg>\t\t\tCreates the given datastructure\n"); */
-/* 		printf("\nYou can also run 'help <command>' on any of the above commands\n"); */
-/* 	} */			 
-/* } */
--- src/ksh_execute.c
+++ src/ksh_execute.c
@@ -7,35 +7,34 @@
 int ksh_help(char **args);
 int ksh_cd  (char **args);
 int ksh_exit(char **args);
+int ksh_pwd (char **args);
 
 // array of all command strings
 char *builtin_str[] = {
     "help",
     "cd",
-    "exit"
+    "exit",
+    "pwd"
 };
 
 // array of all command function pointers
 int (*builtin_func[]) (char **) = {
     &ksh_help,
     &ksh_cd,
-    &ksh_exit
+    &ksh_exit,
+    &ksh_pwd
 };
 
-// returns the number of commands
-int ksh_num_cmds()
-{
-    return sizeof(builtin_str) / sizeof(char *);
-}
 
-// returns  1 if program executes properly
-//          -1 if program fails to execute properly
+// execute the first command in args
+// returns  0 if program executes properly
+//          ERR if program fails to execute properly
 int ksh_execute(char **args)
 {
     int i;
 
     if(args[0] == NULL){
-        return -1;
+        return ERR;
     }
 
     for(i = 0; i < ksh_num_cmds(); i++){
@@ -48,17 +47,62 @@ int ksh_execute(char **args)
 }
 
 
+// returns the number of commands
+int ksh_num_cmds()
+{
+    return sizeof(builtin_str) / sizeof(char *);
+}
+
+// help
 int ksh_help(char **args)
 {
+    if(args[1] == NULL){
+        printf("===========================================\n");
+        printf(" ksh - A simple shell created from scratch\n");
+        printf("       in C\n");
+        printf("===========================================\n");
+        printf("                              By: Kyle Pollina\n");
+        printf("These are the commands you can run...\n\n");
+
+        printf("<command>\t\t<description>\n");
+        printf("help\t\t\topen help list\n");
+        printf("cd <path>\t\tchange directory\n");
+        printf("exit\t\t\tclose shell\n");
+    }
+
+    else{
+        if(strcmp(args[1], "help") == 0){
+            printf("lsh: help <command> - get more info on <command>\n");
+        }
+        else if(strcmp(args[1], "cd") == 0){
+            printf("lsh: cd <path> - change directory to <path>\n");
+        }
+        else if(strcmp(args[1], "exit") == 0){
+            printf("lsh: exit - exit the current iteration of ksh\n");
+        }
+    }
+
     return 0;
 }
 
+// change directory
 int ksh_cd(char **args)
 {
+    if(args[1] == NULL){
+        fprintf(stderr, "lsh: you need to put a path after cd dummy. type 'help cd' for more help\n");
+    }
+    else{
+        if(chdir(args[1]) != 0){
+            perror("lsh");
+        } 
+    }
+
     return 0;
 }
 
+// exit
 int ksh_exit(char **args)
 {
-    return 0;
+    return 1;
 }
+
